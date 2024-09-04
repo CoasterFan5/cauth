840fdfd81dd06eeff38e8d33dc7834dfb5f9f0b1
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-09-04 15:36:29 -0400

    add missing layout file

---
+++ src/routes/dashboard/+layout.server.ts
@@ -0,0 +1,23 @@
+import { verifySession } from '$lib/server/verifySession.js';
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ cookies }) => {
+    const user = await verifySession(cookies.get('session'));
+    if (!user) {
+        throw redirect(303, '/');
+    }
+
+    if (!user.verifiedEmail) {
+        throw redirect(303, '/verify-email');
+    }
+
+    return {
+        user: {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            verifiedEmail: user.verifiedEmail
+        }
+    };
+};
